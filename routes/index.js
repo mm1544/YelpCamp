02cc71411e9c44e2c51463252511ae028bf0b05a
M. Minskis <martynas.minskis@gmail.com>
2019-10-24 02:21:01 +0100

    add user_profiles

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ var router = express.Router();
 // now will add all the route
 var passport = require("passport");
 var User = require("../models/user");
+var Campground = require("../models/campground");
+
+
  
 // "Landing page" is going to be on the root path:
 router.get("/", function(req, res){
@@ -23,7 +26,13 @@ router.get("/register", function(req, res){
 
 // Handle sign-up logic
 router.post("/register", function(req, res){
-	var newUser = new User({username: req.body.username});
+	var newUser = new User({
+		username: req.body.username, 
+		firstName: req.body.firstName, 
+		lastName: req.body.lastName,
+		email: req.body.email,
+		avatar: req.body.avatar
+	});
 	if(req.body.adminCode === "secret123"){
 		newUser.isAdmin = true;
 	}
@@ -74,6 +83,24 @@ router.get("/logout", function(req, res){
 // 	First thing that we are passing is a key, it can be anything..
 	req.flash("success", "Logged you out!");
 	res.redirect("/campgrounds");
+});
+
+
+// User profiles
+router.get("/users/:id", function(req, res){
+	// "/:id" is accessed/passed by/to req.params.id
+	User.findById(req.params.id, function(err, foundUser){
+		if(err || !foundUser){
+			return res.redirect("/campgrounds");
+		}
+		Campground.find().where("author.id").equals(foundUser._id).exec(function(err, campgrounds){
+			if(err || !campgrounds){
+				return res.redirect("/campgrounds");
+			}
+			res.render("users/show", {user: foundUser, campgrounds: campgrounds});
+		});
+		
+	});
 });
 
 
